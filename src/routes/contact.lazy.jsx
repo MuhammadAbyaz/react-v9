868580681e0cf74a814a587af185e2567141cbd2
MuhammadAbyaz <m.abyaz681@gmail.com>
2024-12-31 19:00:04 +0500

    fix: destructure pending from useFormStatus

--- src/routes/contact.lazy.jsx
+++ src/routes/contact.lazy.jsx
@@ -34,7 +34,7 @@ function ContactUs() {
 }
 
 function ContactInput(props) {
-  const [pending] = useFormStatus();
+  const { pending } = useFormStatus();
   return (
     <input
       disabled={pending}
